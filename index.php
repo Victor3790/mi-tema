642fa51ae6092e66cf1f141d8933c3bcc6ec4b5a
victor <victor3790@gmail.com>
2022-07-24 00:00:23 -0500

    header styles added

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     <!-- Page header with logo and tagline-->
     <header class="py-5 bg-light border-bottom mb-4 hero">
         <div class="container">
-            <div class="text-center my-5">
+            <div class="text-center my-5 hero__text">
                 <h1 class="fw-bolder">Welcome to Blog Home!</h1>
                 <p class="lead mb-0">A Bootstrap 5 starter layout for your next blog homepage</p>
             </div>

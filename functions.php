18d6e2db00c751850840f681e51c1947785d6ac3
victor <victor3790@gmail.com>
2022-07-23 22:10:59 -0500

    Bootstrap 1

--- functions.php
+++ functions.php
@@ -5,7 +5,22 @@ add_action( 'wp_enqueue_scripts', 'add_assets' );
 function add_assets()
 {
 
-    wp_enqueue_style( 'my-blog-style', get_stylesheet_uri() );
-    wp_enqueue_script( 'my-blog-script', get_template_directory_uri().'/js/script.js', array(), false, true );
+    wp_enqueue_style( 
+        'my-blog-style', 
+        get_stylesheet_uri() 
+    );
+
+    wp_enqueue_style( 
+        'my-blog-home-style', 
+        get_template_directory_uri() . '/css/home.css' 
+    );
+
+    wp_enqueue_script( 
+        'my-blog-bootstrap', 
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js', 
+        array(), 
+        false, 
+        true 
+    );
 
 }

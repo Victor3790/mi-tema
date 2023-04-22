cba727a21045bb267cfd5266d7a355157b16caed
victor <victor3790@gmail.com>
2023-04-22 08:51:08 -0600

    Support for tags added

--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -6,9 +6,18 @@
         <h1 class="fw-bolder mb-1"><?php the_title(); ?></h1>
         <!-- Post meta content-->
         <div class="text-muted fst-italic mb-2">Publicado el <?php the_date(); ?> by <?php the_author(); ?></div>
-        <!-- Post categories-->
-        <a class="badge bg-secondary text-decoration-none link-light" href="#!">Web Design</a>
-        <a class="badge bg-secondary text-decoration-none link-light" href="#!">Freebies</a>
+        <!-- Post tags-->
+        <?php
+            $tags = get_the_tags();
+
+            if( $tags && ! is_wp_error($tags)):
+                foreach( $tags as $tag ) :
+        ?>
+            <a class="badge bg-secondary text-decoration-none link-light" href="<?php echo get_tag_link($tag); ?>"><?php echo $tag->name; ?></a>
+        <?php
+                endforeach;
+            endif;
+        ?>
     </header>
     <!-- Preview image figure-->
     <figure class="mb-4"><?php the_post_thumbnail( 'my_theme_feature_image', [ 'class' => 'img-fluid' ] ); ?></figure>
